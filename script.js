1e490b11f0212b1b2c72707fe7dabc4b8cbcf78d
praveen6228 <104729165+praveen6228@users.noreply.github.com>
2023-01-13 14:23:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
 function palindrome(str){
+	let reverse = str.split(" ").reverse().join(" ");
+	return str.toLowerCase() === reverse.toLowerCase();
 
 }
+console.log(palindrome("racecar"))
 module.exports = palindrome
